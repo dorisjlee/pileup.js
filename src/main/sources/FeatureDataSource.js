23d217a49c23ae897ae6160aa22d92c66548c6e5
justinzh1 <justinzhongvs@gmail.com>
2016-06-28 12:59:11 -0700

    finished feature.js featuredatasource.js and featuredatasource-test.js

--- src/main/sources/FeatureDataSource.js
+++ src/main/sources/FeatureDataSource.js
@@ -1,42 +1,18 @@
-/* @flow */
+  /** @flow */
 'use strict';
 
-// import type {Gene, BigBedSource} from './BigBedDataSource';
-import type {BigBedSource} from './BigBedDataSource';
+import BigBedSource from './BigBedDataSource';
 import ContigInterval from '../ContigInterval';
-// import BigBed from '../data/BigBed';
 import RemoteRequest from '../RemoteRequest';
 import FeatureEndpoint from '../data/FeatureEndpoint';
 import Interval from '../Interval';
 import Q from 'q';
 import _ from 'underscore';
-import {Events} from 'backbone';
+import { Events } from 'backbone';
 
-var FEATURES_PER_REQUEST = 200; //TODO Arbitrary number for now
+var FEATURES_PER_REQUEST = 200;
 
-// function parseBedFeature(f): Feature {
-//   var contig = new ContigInterval(f.contig, f.start, f.stop);
-//   var position = new ContigInterval(f.contig, f.start, f.stop),
-//       x = f.rest.split('\t'),
-//       // exons arrays sometimes have trailing commas
-//       exonLengths = x[7].replace(/,*$/, '').split(',').map(Number),
-//       exonStarts = x[8].replace(/,*$/, '').split(',').map(Number),
-//       exons = _.zip(exonStarts, exonLengths)
-//                .map(function([start, length]) {
-//                  return new Interval(f.start + start, f.start + start + length);
-//                });
-//   //TODO replace this JSON format with the proper one needed for Feature object
-//   return {
-//     range: contig,
-//     featureId: x[0],  // e.g. ENST00000359597
-//     featureType: x[1],
-//     start: x[2],
-//     end: x[3],
-//     exons
-//   };
-// }
-
-var createFromFeatureEndpoint = function(remoteSource: FeatureEndpoint): BigBedSource {
+function createFromFeatureEndpoint(remoteSource: FeatureEndpoint): BigBedSource {
     // Collection of genes that have already been loaded.
   var features: {[key:string]: feature} = {}; //TODO features
 
@@ -110,7 +86,7 @@ var createFromFeatureEndpoint = function(remoteSource: FeatureEndpoint): BigBedS
   _.extend(o, Events);  // Make this an event emitter
 
   return o;
-}
+};
 
 function create(data: {url: string}): BigBedSource {
       var url = data.url;
